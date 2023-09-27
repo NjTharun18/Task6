42547787f9582565cf5e7ffdfaeab9c3536fea9d
NjTharun18 <146088333+NjTharun18@users.noreply.github.com>
2023-09-27 17:04:21 +0530

    Add files via upload

---
+++ 27-9-23/main.js
@@ -0,0 +1,32 @@
+var sport="Cricket";
+function varExample(){
+    if(true){
+        var name="Tharun";
+        console.log(name);
+        console.log(sport);
+        name="Virat";
+        console.log(sport);
+        console.log(name);
+    }
+    console.log(name);
+}
+varExample();
+function letEx(){
+    if(true){
+        let a="Bangalore";
+        console.log(a);
+        a="Andhra";
+        console.log(a);
+    }
+}
+letEx();
+const c=20;
+    function constExample(){
+        if(true){
+            const b=10;
+            console.log(b);
+            console.log(c);
+        }
+        
+    }
+constExample();
